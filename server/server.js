07eb6e669955321222c0c964ddf40201c42cd496
ikramhasib007 <ikramhasib007@gmail.com>
2018-08-31 15:21:32 +0600

    generate io.emit event for all users

--- server/server.js
+++ server/server.js
@@ -38,93 +38,89 @@ io.on('connection', (socket) => {
         return console.log(err);
     });
 
-    app.get('/', (req, res) => {
-        res.render('home.hbs');
-    });
-    
-    app.get('/book/add', (req, res) => {
-        res.render('book/add.hbs');
-    });
-    
-    app.post('/book/save', (req, res) => {
-        if(!req.body) {
-            return false;
-        }
-        var book = new Book({
-            title: req.body.title,
-            author: req.body.author,
-            fee: req.body.fee,
-            about_author: req.body.about_author
-        });
-    
-        book.save().then((book) => {
-            res.redirect('/book/list');
-        }, (err) => {
-            return console.log(err);
-        })
-    });
-    
-    app.get('/book/list', (req, res) => {
-        res.render('book/list.hbs');
-    });
-    
-    app.get('/book/borrow/:id', (req, res) => {
-        var id = req.params.id;
-    
-        if(!ObjectID.isValid(id)) {
-            res.redirect('/book/list');
-        }
-        Book.findById(id).then(book => {
-            book.delivery_charges = delivery_charges;
-            res.render('book/details.hbs', {book}, () => {
-                socket.broadcast.emit('outOfStock', book);
-                res.send(book);
-            });
-        }, err => {
-            return console.log(err);
-        });
-    });
-    
-    app.post('/book/cart', (req, res) => {
-        if(!req.body)  {
-            return false;
-        }
-        var id = req.body.id;
-        if(!ObjectID.isValid(id)) {
-            res.redirect('/book/list');
-        }
-        Book.findById(id).then(book => {
-            console.log(book);
-            if(book.status === 'out of stock') {
-                res.redirect('/book/list');
-            }
-            book.delivery_charges = delivery_charges;
-            book.total_fees = book.delivery_charges + book.fee;
-            res.render('book/cart.hbs', {book});
-        }, err => {
-            return console.log(err);
-        });
-        
-    });
-
-    app.post('/book/checkout', (req, res) => {
-        if(!req.body) {
-            return false;
-        }
-        var id = req.body.id;
-        if(!ObjectID.isValid(id)) {
-            res.redirect('/book/list');
-        }
-        var body = {
-            status: 'out of stock'
-        };
-        Book.findByIdAndUpdate(id, {$set:body}, {new: true}).then(book => {
-            console.log('updated',book);
-            res.redirect('/book/list');
-        });
-    });
-
 });
+
+app.get('/', (req, res) => {
+    res.render('home.hbs');
+});
+
+app.get('/book/add', (req, res) => {
+    res.render('book/add.hbs');
+});
+
+app.post('/book/save', (req, res) => {
+    if(!req.body) {
+        return false;
+    }
+    var book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        fee: req.body.fee,
+        about_author: req.body.about_author
+    });
+
+    book.save().then((book) => {
+        res.redirect('/book/list');
+    }, (err) => {
+        return console.log(err);
+    })
+});
+
+app.get('/book/list', (req, res) => {
+    res.render('book/list.hbs');
+});
+
+app.get('/book/borrow/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        res.redirect('/book/list');
+    }
+    Book.findById(id).then(book => {
+        book.delivery_charges = delivery_charges;
+        res.render('book/details.hbs', {book});
+    }, err => {
+        return console.log(err);
+    });
+});
+
+app.post('/book/cart', (req, res) => {
+    if(!req.body)  {
+        return false;
+    }
+    var id = req.body.id;
+    if(!ObjectID.isValid(id)) {
+        res.redirect('/book/list');
+    }
+    Book.findById(id).then(book => {
+        if(book.status === 'out of stock') {
+            res.redirect('/book/list');
+        }
+        book.delivery_charges = delivery_charges;
+        book.total_fees = book.delivery_charges + book.fee;
+        res.render('book/cart.hbs', {book});
+    }, err => {
+        return console.log(err);
+    });
+    
+});
+
+app.post('/book/checkout', (req, res) => {
+    if(!req.body) {
+        return false;
+    }
+    var id = req.body.id;
+    if(!ObjectID.isValid(id)) {
+        res.redirect('/book/list');
+    }
+    var body = {
+        status: 'out of stock'
+    };
+    Book.findByIdAndUpdate(id, {$set:body}, {new: true}).then(book => {
+        res.redirect('/book/list');
+    });
+});
+
 
 
 
